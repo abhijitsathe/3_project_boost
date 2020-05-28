91b116e07e6dacaa20ea740cce88c2cca0702b46
abhijitsathe <36135306+abhijitsathe@users.noreply.github.com>
2020-05-28 18:53:18 -0400

    added obstacles and rocket rcs thrust

---
+++ Assets/Rocket.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField]float rcsThrust = 100f;
+    Rigidbody rightBody;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rightBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rightBody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        rightBody.freezeRotation = false; 
+    }
+}
